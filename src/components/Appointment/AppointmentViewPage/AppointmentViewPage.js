d7c840fc12719b4f750ee1b6420dccc63c3cb213
Joseph Ugiagbe <blessedjoe2000@yahoo.com>
2022-11-19 10:03:52 -0600

    master edited

--- src/components/Appointment/AppointmentViewPage/AppointmentViewPage.js
+++
@@ -1,9 +0,0 @@
-const AppointmentView = () => {
-  return (
-    <div className="appointment-home">
-      <p>Your next appointment is ...date</p>
-    </div>
-  );
-};
-
-export default AppointmentView;
