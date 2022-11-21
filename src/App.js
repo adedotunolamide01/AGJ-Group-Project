67d866cab57cb19676a257ef0708540f71cc56d5
Joseph Ugiagbe <blessedjoe2000@yahoo.com>
2022-11-21 11:54:28 -0600

    health page created, diabetes, cancer, stroke page completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import AppointmentHome from "./components/Appointment/AppointmentHomePage/AppointmentHomePage";
 import BookAppointment from "./components/Appointment/BookAppointment.js/BookAppointment";
 import ViewAppointment from "./components/Appointment/ViewAppointment/ViewAppointment";
+import Cancer from "./components/HealthInformation/Cancer/Cancer";
+import Diabetes from "./components/HealthInformation/Diabetes/Diabetes";
+import HealthInformation from "./components/HealthInformation/HealthInfoHomePage/HealthInfo";
+import Stroke from "./components/HealthInformation/Stroke/Stroke";
 import Doctorpage from "./components/Page/Doctorpage";
 import Frontpage from "./components/Page/FrontPage";
 
@@ -15,6 +19,10 @@ function App() {
         <Route path="/appointment" element={<AppointmentHome />} />
         <Route path="/viewappointment" element={<ViewAppointment />} />
         <Route path="/bookappointment" element={<BookAppointment />} />
+        <Route path="/healthinfo" element={<HealthInformation />} />
+        <Route path="/diabetes" element={<Diabetes />} />
+        <Route path="/cancer" element={<Cancer />} />
+        <Route path="/stroke" element={<Stroke />} />
       </Routes>
     </div>
   );
